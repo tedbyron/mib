21a1756abdf57d083ff02eff23043e9c527b6cd1
Teddy Byron <ted@tedbyron.com>
2022-07-12 01:34:06 -0400

    more uart

--- uart/src/main.rs
+++ uart/src/main.rs
@@ -1,11 +1,14 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 use cortex_m_rt::entry;
+use heapless::Vec;
 use microbit::hal::prelude::*;
 use microbit::hal::uarte::{self, Baudrate, Parity};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 
 mod serial_setup;
 use serial_setup::UartePort;
@@ -22,9 +25,30 @@ fn main() -> ! {
         Baudrate::BAUD115200,
     );
     let mut uarte = UartePort::new(serial);
+    let mut buf = Vec::<_, 64>::new();
 
     loop {
-        let b = nb::block!(uarte.read()).unwrap();
-        rprintln!("{}", char::from(b));
+        buf.clear();
+
+        loop {
+            let b = nb::block!(uarte.read()).unwrap();
+            nb::block!(uarte.write(b)).unwrap();
+            nb::block!(uarte.flush()).unwrap();
+
+            if buf.push(b).is_err() {
+                write!(uarte, "uh oh buffer full\r\n").unwrap();
+                break;
+            }
+
+            if b == b'\r' || b == b'\n' {
+                write!(uarte, "\r\n").unwrap();
+                for &b in buf.iter().rev().chain(&[b'\r', b'\n']) {
+                    nb::block!(uarte.write(b)).unwrap();
+                }
+                break;
+            }
+        }
+
+        nb::block!(uarte.flush()).unwrap();
     }
 }
